9fdef2c4eda03dc2096ba1fec04ba1738ad0605a
Sebastian Velo <sebastian.velo@mercadolibre.com>
2021-10-02 22:32:59 -0300

    Styles updated

--- src/style/ComponentStyle.ts
+++ src/style/ComponentStyle.ts
@@ -1,14 +1,13 @@
 enum ComponentStyle {
-    NAVIGATION = `flex bg-primary justify-between items-center w-screen py-2 px-4`,
-    NAVIGATION_HEADER = `text-2xl`,
-    NAVIGATION_ACTIONS = ``,
-
     ACTION = `px-4 py-2 rounded-md`,
-
     PILL = `py-2 px-4 shadow-md rounded-full font-semibold text-sm mr-2 w-max`,
     DROPDOWN = `list-none`,
     SPINNER_OUTER = "ease-linear rounded-full h-20 w-20 relative border-4 border-primary-light",
     SPINNER_INNER = "ease-linear rounded-full h-20 w-20 absolute -top-1 -left-1 border-t-8 border-b-0 border-l-4 border-r-0 border-primary-dark animate-spin",
+
+    NAVIGATION = `flex bg-primary justify-between items-center w-screen py-2 px-4`,
+    NAVIGATION_HEADER = `text-2xl`,
+    NAVIGATION_ACTIONS = ``,
 }
 
 export default ComponentStyle;
