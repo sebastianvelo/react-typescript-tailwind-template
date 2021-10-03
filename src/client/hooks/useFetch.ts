29814bb1c4d5a0a9a97d7c4fcea1efb5f4e93f8f
Sebastian Velo <sebastian.velo@mercadolibre.com>
2021-10-03 10:19:24 -0300

    New hook useFetch was added and axios was installed

---
+++ src/client/hooks/useFetch.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { useState, useEffect } from "react";
+
+export const useFetch = <T extends Object>(req: AxiosRequestConfig) => {
+  const [data, setData] = useState<T>();
+
+  useEffect(() => {
+    axios
+      .request(req)
+      .then(function (response) {
+        setData(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
+  return data;
+};
